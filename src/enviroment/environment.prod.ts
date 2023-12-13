c4eb65114e262a34921070269cdcc6900c341dbf
Andres Baez <136645843+andresxyk@users.noreply.github.com>
2023-12-13 12:39:29 -0600

    add enviroment prod

---
+++ src/enviroment/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  URLEspirometro: "msesp-espirometro",
+  URLGenerales: "msesp-espirometro",
+  URLGestion: "msccm-encuesta",
+  URLConsultaReporte: "msesp-consulta-Reporte",
+  URLCargaXML: "msesp-nss-cargaXML",
+  URLCargaPDF: "msesp-cargaPDF",
+  URLManuales: "espirome-files/manuales",
+  URLOauth:"msesp-sist-Oauth",
+  URLFiles: ""
+};
